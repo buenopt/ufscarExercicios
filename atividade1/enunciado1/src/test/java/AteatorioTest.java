6c1fbe8e093f2b32d0141ca3cbb1ad9fdd7d3734
Fernando Bueno <BUENOPT@HOTMAIL.COM>
2023-06-27 23:02:38 -0300

    atividade2
    Teste de unidade

---
+++ atividade1/enunciado1/src/test/java/AteatorioTest.java
@@ -0,0 +1,41 @@
+
+import bueno.fernando.enunciado1.Aleatorio;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author fbueno
+ */
+public class AteatorioTest {
+    
+    @Test
+    public void testGerarNumeroAleatorio() {
+        Aleatorio aleatorio = new Aleatorio();
+
+        // Testando intervalo válido
+        int inicio = 1;
+        int fim = 10;
+        int resultado = aleatorio.gerarNumeroAleatorio(inicio, fim);
+        Assertions.assertTrue(resultado >= inicio && resultado <= fim);
+
+        // Testando intervalo inválido (início maior ou igual ao fim)
+        inicio = 10;
+        fim = 1;
+        resultado = aleatorio.gerarNumeroAleatorio(inicio, fim);
+        Assertions.assertEquals(-1, resultado);
+
+        // Testando início do intervalo negativo
+        inicio = -5;
+        fim = 10;
+        resultado = aleatorio.gerarNumeroAleatorio(inicio, fim);
+        Assertions.assertEquals(-1, resultado);
+
+        // Testando fim do intervalo negativo
+        inicio = 1;
+        fim = -10;
+        resultado = aleatorio.gerarNumeroAleatorio(inicio, fim);
+        Assertions.assertEquals(-1, resultado);
+    }
+    
+}
---
+++ atividade1/enunciado2/src/main/java/bueno/fernando/enunciado2/Enunciado2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bueno.fernando.enunciado2;
+
+/**
+ *
+ * @author fbueno
+ */
+public class Enunciado2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
